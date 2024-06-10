4e247e8e13d81c38c7b10a6622d396ef949759d0
alviasad0 <alviasad10.com>
2024-06-10 08:31:46 +0600

    added the error page

--- src/Pages/Home Page/Compoents/Announcements.jsx
+++ src/Pages/Home Page/Compoents/Announcements.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const Announcements = () => {
 
@@ -13,12 +14,14 @@ const Announcements = () => {
     return (
       <div>
         <div className="w-full h-10 bg-[#646CFF]">
-          <div className="max-w-screen-2xl mx-auto flex ">
+          <div className="max-w-screen-2xl mx-auto  ">
             <Marquee className="text-white text-xl font-bold">
               {allData.map((data) => (
                 <div key={data._id}>
                   <p className="text-lg font-medium pt-2 mr-16">
-                    ***{data.announcement}***
+                    <Link to={`/announcementDetails/${data._id}`}>
+                      ***{data.announcement}***
+                    </Link>
                   </p>
                 </div>
               ))}
